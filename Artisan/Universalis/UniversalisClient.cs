32369f3758f077e7e02528f338a8f7dd81736e48
PartyLich <PartyLich@gmail.com>
2023-12-31 15:13:32 -0600

    feat: request list of items from Universalis
    Previous implementation made an individual network roundtrip for every
    single item in the ingredient list. This change makes a single request
    for the entire list.

--- Artisan/Universalis/UniversalisClient.cs
+++ Artisan/Universalis/UniversalisClient.cs
@@ -3,6 +3,7 @@ using ECommons;
 using ECommons.DalamudServices;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -23,7 +24,7 @@ namespace Artisan.Universalis
             };
         }
 
-        public MarketboardData? GetMarketBoard(string region, ulong itemId)
+        public List<MarketboardData?> GetMarketBoard(string region, IEnumerable<ulong> itemId)
         {
             var marketBoardFromAPI = this.GetMarketBoardData(region, itemId);
             return marketBoardFromAPI;
@@ -40,7 +41,20 @@ namespace Artisan.Universalis
                 return null;
 
 
-            return GetMarketBoard(region, itemId);
+            return GetMarketBoard(region, [itemId])[0];
+        }
+
+        public List<MarketboardData?>? GetRegionData(IEnumerable<ulong> itemIds)
+        {
+            var world = Svc.ClientState.LocalPlayer?.CurrentWorld.Id;
+            if (world == null)
+                return null;
+
+            var region = Regions.GetRegionByWorld(world.Value);
+            if (region == null)
+                return null;
+
+            return GetMarketBoard(region, itemIds);
         }
 
         public MarketboardData? GetDataCenterData(ulong itemId)
@@ -53,7 +67,20 @@ namespace Artisan.Universalis
             if (datacenter == null)
                 return null;
 
-            return GetMarketBoard(datacenter, itemId);
+            return GetMarketBoard(datacenter, [itemId])[0];
+        }
+
+        public List<MarketboardData?>? GetDataCenterData(IEnumerable<ulong> itemIds)
+        {
+            var world = Svc.ClientState.LocalPlayer?.CurrentWorld.Id;
+            if (world == null)
+                return null;
+
+            var datacenter = DataCenters.GetDataCenterNameByWorld(world.Value);
+            if (datacenter == null)
+                return null;
+
+            return GetMarketBoard(datacenter, itemIds);
         }
 
         public void Dispose()
@@ -102,12 +129,14 @@ namespace Artisan.Universalis
             return marketBoardData;
         }
 
-        private MarketboardData? GetMarketBoardData(string region, ulong itemId)
+        private List<MarketboardData?>? GetMarketBoardData(string region, IEnumerable<ulong> itemIds)
         {
+            if (!itemIds.Any()) return null;
+
             HttpResponseMessage result;
             try
             {
-                result = this.GetMarketBoardDataAsync(region, itemId).Result;
+                result = this.GetMarketBoardDataAsync(region, itemIds).Result;
             }
             catch (Exception ex)
             {
@@ -120,7 +149,7 @@ namespace Artisan.Universalis
             {
                 PluginLog.LogError(
                     "Failed to retrieve data from Universalis for itemId {0} / worldId {1} with HttpStatusCode {2}.",
-                    itemId,
+                    String.Join(",", itemIds),
                     region,
                     result.StatusCode);
                 return null;
@@ -131,29 +160,31 @@ namespace Artisan.Universalis
             {
                 PluginLog.LogError(
                     "Failed to deserialize Universalis response for itemId {0} / worldId {1}.",
-                    itemId,
+                     String.Join(",", itemIds),
                     region);
                 return null;
             }
 
             try
             {
-                return ParseMarketData(json);
+                return (itemIds.Count() == 1)
+                    ? [ParseMarketData(json)]
+                    : itemIds.Select<ulong, MarketboardData?>(id => ParseMarketData(json.items[$"{id}"])).ToList();
             }
             catch (Exception ex)
             {
                 PluginLog.LogError(
                     ex,
                     "Failed to parse marketBoard data for itemId {0} / worldId {1}.",
-                    itemId,
+                    String.Join(",", itemIds),
                     region);
                 return null;
             }
         }
 
-        private async Task<HttpResponseMessage> GetMarketBoardDataAsync(string? worldId, ulong itemId)
+        private async Task<HttpResponseMessage> GetMarketBoardDataAsync(string? worldId, IEnumerable<ulong> itemIds)
         {
-            var request = Endpoint + worldId + "/" + itemId;
+            var request = Endpoint + worldId + "/" + String.Join(",", itemIds);
             PluginLog.LogDebug($"universalisRequest={request}");
             return await this.httpClient.GetAsync(new Uri(request));
         }
